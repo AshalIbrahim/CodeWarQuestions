b01b92326ddf7c2ef49fdc695c8842a521647b97
AshalIbrahim <113935461+AshalIbrahim@users.noreply.github.com>
2024-06-24 00:27:17 +0500

    Update Q1.java
    We need to sum big numbers and we require your help.
    
    Write a function that returns the sum of two numbers. The input numbers are strings and the function must return a string.
    
    Example
    add("123", "321"); -> "444"
    add("11", "99");   -> "110"
    Notes
    The input numbers are big.
    The input is a string of only digits
    The numbers are positives

--- Q1.java
+++ Q1.java
@@ -1,5 +1,17 @@
 package CodeWarsQuestions;
 
+// We need to sum big numbers and we require your help.
+
+// Write a function that returns the sum of two numbers. The input numbers are strings and the function must return a string.
+
+// Example
+// add("123", "321"); -> "444"
+// add("11", "99");   -> "110"
+// Notes
+// The input numbers are big.
+// The input is a string of only digits
+// The numbers are positives
+
 public class Q1 {
 
     public static String add(String a, String b) {
